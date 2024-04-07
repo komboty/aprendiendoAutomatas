785a41ded3f080c6b9f34987f15a7446eaebff18
komboty <komubk@gmail.com>
2024-04-06 20:06:34 -0600

    ADD: Nivel 1 con suelo y jugador con movimiento en X y Z

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Clase que sirve para eliminar hardcore.
+/// </summary>
 public class Constants : MonoBehaviour
 {
+    /// <summary>
+    /// Nombres de Escenas.
+    /// </summary>
     public string nameSceneMenu = "MainMenu";
     public string nameSceneNivel1 = "Nivel1";
+
+    /// <summary>
+    /// Nombres de Inputs.
+    /// </summary>
+    public string nameGetAxisX = "Horizontal";
+    public string nameGetAxisZ = "Vertical";
+
+    /// </summary>
+    /// Valores default del jugador.
+    /// </summary>
+    public float speedPlayer = 5f;
+
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Clase que controla el funcionamiento del Menu.
+/// </summary>
 public class MenuManager : MonoBehaviour 
 {
+    // Variable con constantes de todo el juego.
     public Constants constants;
 
+    /// <summary>
+    /// Inicia el juego.
+    /// </summary>
     public void OnPlay()
     {
         SceneManager.LoadScene(constants.nameSceneNivel1);
     }
 
+    /// <summary>
+    /// Finaliza el juego.
+    /// </summary>
     public void OnExit()
     {
         Application.Quit();
